92e739816d41e1b58e06387fe94d0c819074dfff
MarynaFundament <113469828+MarynaFundament@users.noreply.github.com>
2024-02-12 13:30:21 +0000

    Deploying to gh-pages from @ MarynaFundament/hw-from8@1794393ff884aac4af19b706addee4a27966423d 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{d as a}from"./assets/vendor-b0b9f23b.js";const t="feedback-form-state",o=document.querySelector(".feedback-form");o.addEventListener("submit",e=>e.preventDefault());o.addEventListener("input",a(e=>{const r=e.target.value;localStorage.setItem(t,r),localStorage.getItem(t)},1e3));
+//# sourceMappingURL=commonHelpers2.js.map
